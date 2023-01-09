b21be3218fdc69f2b2028ea036ed7b8e5081e004
Tim Frison <frison@gmail.com>
2023-01-08 19:47:43 -0700

    Auto-pull git repo on interval

--- runtime/test.sh
+++ runtime/test.sh
@@ -3,9 +3,11 @@
 make build
 
 docker run -it \
-  -e SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" \
+  -e SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" \
   -e WALKER_GIT_REPO="git@github.com:frison/_slash.git" \
   -e WALKER_GIT_REF="main" \
-  -e OTM_HOST="ofthemachine-curation.herokuapp.com" \
+  -e WALKER_UPSTREAM_HOST="ofthemachine-curation.herokuapp.com" \
+  -e FETCH_AFTER_COUNT=5 \
+  -e WALKER_STEP_INTERVAL_IN_SEC=1 \
    runtime-walker:local \
    "./services/data/postgres/files/bin/connect.sh"
--- runtime/walker/files/entrypoint.sh
+++ runtime/walker/files/entrypoint.sh
@@ -7,7 +7,8 @@ validate_env_exists "WALKER_GIT_REPO" "to checkout the walker repo"
 validate_env_exists "WALKER_GIT_REF" "to checkout the desired version of the walker repo"
 
 export WALKER_UPSTREAM_HOST=${WALKER_UPSTREAM_HOST:-"ofthemachine-curation.herokuapp.com"}
-export WALKER_STEP_INTERVAL=${WALKER_STEP_INTERVAL:-5}
+export WALKER_STEP_INTERVAL_IN_SEC=${WALKER_STEP_INTERVAL_IN_SEC:-5}
+export FETCH_AFTER_COUNT=${FETCH_AFTER_COUNT:-10}
 
 . /shell_utils/workdir
 
@@ -21,21 +22,34 @@ cd $WORK_DIR
 git init
 git remote add origin $WALKER_GIT_REPO
 
-# Skipping host checking would require adding github to known hosts prior to checkout
-# Which is doable, but not worth the effort atm
-GIT_SSH_COMMAND="ssh -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no" git fetch --depth 1 origin $WALKER_GIT_REF
+pull() {
+  # Skipping host checking would require adding github to known hosts prior to checkout
+  # Which is doable, but not worth the effort atm
+  GIT_SSH_COMMAND="ssh -i $SSH_PRIVATE_KEY_PATH -o StrictHostKeyChecking=no" git fetch --depth 1 origin $WALKER_GIT_REF
+
+  # We fetched it, now we check it out.
+  git checkout $WALKER_GIT_REF
+}
+
+pull
 
 # Want to nuke this to prevent exfil of the private key from walkers
-rm $SSH_PRIVATE_KEY_PATH
-unset SSH_PRIVATE_KEY
+# SECURITY: The ssh key is only for pulling repos -- or at least should be. We keep it around
+# so the walker can update itself
+#
+# rm $SSH_PRIVATE_KEY_PATH
+# unset SSH_PRIVATE_KEY
 
-# We fetched it, now we check it out.
-git checkout $WALKER_GIT_REF
 
 trap "STOP=1" SIGINT SIGTERM
 
+COUNTER=1
 # Start the first process
 while [[ $STOP -eq 0 ]]; do
+    if [ $(( $COUNTER  % $FETCH_AFTER_COUNT)) -eq 0 ]; then
+        pull
+    fi
     sh -c "$@"
-    sleep $WALKER_STEP_INTERVAL
+    sleep $WALKER_STEP_INTERVAL_IN_SEC
+    let COUNTER=COUNTER+1
 done
