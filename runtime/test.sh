c295e55a574399ed08a723a4863dcf794e3c5dba
Tim Frison <frison@gmail.com>
2023-01-10 23:48:18 -0700

    Include GIT_REF and GIT_REPO for walker

--- runtime/test.sh
+++ runtime/test.sh
@@ -4,10 +4,10 @@ make build
 
 docker run -it \
   -e SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" \
-  -e WALKER_GIT_REPO="git@github.com:frison/ofthemachine.git" \
+  -e WALKER_GIT_REPO="git@github.com:frison/_slash.git" \
   -e WALKER_GIT_REF="main" \
   -e WALKER_UPSTREAM_HOST="ofthemachine-curation.herokuapp.com" \
   -e FETCH_AFTER_COUNT=5 \
   -e WALKER_STEP_INTERVAL_IN_SEC=1 \
    runtime-walker:local \
-   "./services/data/postgres/files/bin/connect.sh"
+   "printenv"
--- runtime/walker/files/entrypoint.sh
+++ runtime/walker/files/entrypoint.sh
@@ -9,6 +9,7 @@ validate_env_exists "WALKER_GIT_REF" "to checkout the desired version of the wal
 export WALKER_UPSTREAM_HOST=${WALKER_UPSTREAM_HOST:-"ofthemachine-curation.herokuapp.com"}
 export WALKER_STEP_INTERVAL_IN_SEC=${WALKER_STEP_INTERVAL_IN_SEC:-5}
 export FETCH_AFTER_COUNT=${FETCH_AFTER_COUNT:-10}
+export GIT_REPO=${WALKER_GIT_REPO}
 
 . /shell_utils/workdir
 
@@ -17,6 +18,10 @@ mkdir -p ~/.ssh
 export SSH_PRIVATE_KEY_PATH=~/.ssh/walker_github_private_key
 echo "$SSH_PRIVATE_KEY" > $SSH_PRIVATE_KEY_PATH
 chmod 600 $SSH_PRIVATE_KEY_PATH
+
+# Unset all env vars that end in _PRIVATE_KEY, ie SSH_PRIVATE_KEY, but a little more aggressive
+# because some k8s env vars have _PRIVATE_KEY in them
+unset $(printenv | cut -f1 -d=  | grep '.*_PRIVATE_KEY$')
 
 cd $WORK_DIR
 git init
@@ -34,6 +39,7 @@ pull() {
   git checkout $WALKER_GIT_REF
   git checkout -b walker-$(date +%s)
   git clean --force -dx
+  export GIT_REF=$(git rev-parse --short HEAD)
 }
 
 pull
@@ -42,14 +48,10 @@ pull
 # SECURITY: The ssh key is only for pulling repos -- or at least should be. We keep it around
 # so the walker can update itself
 #
-# rm $SSH_PRIVATE_KEY_PATH
-unset SSH_PRIVATE_KEY
-
-
 trap "STOP=1" SIGINT SIGTERM
 
 COUNTER=1
-# Start the first process
+
 while [[ $STOP -eq 0 ]]; do
     if [ $(( $COUNTER  % $FETCH_AFTER_COUNT)) -eq 0 ]; then
         pull
